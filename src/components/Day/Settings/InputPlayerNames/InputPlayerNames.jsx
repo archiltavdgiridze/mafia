bb7f4ef6a699eafd4625236fb0f3f3a68537a3f2
jeliaaa <julia.aleqsandre@gmail.com>
2023-09-02 04:05:05 +0400

    made some changes on inputs / waiting for tasks

--- src/components/Day/Settings/InputPlayerNames/InputPlayerNames.jsx
+++ src/components/Day/Settings/InputPlayerNames/InputPlayerNames.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import InputName from "../../../../reComps/InputName/InputName";
 import "./inputplayernames.scss";
 import { Link } from "react-router-dom";
@@ -6,11 +6,33 @@ import stripLeft from "../../../../assets/img/strip-left.svg";
 
 const InputPlayerNames = () => {
   const numOfPlayers = 12;
-
+  const formOfInps = useRef();
+  const [isFormValid, setIsFormValid] = useState(false); // Initialize form validity state
   const inputComponents = Array.from({ length: numOfPlayers }, (_, index) => (
     <InputName key={index} numOfPlayers={index + 1} />
   ));
 
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    // Perform your form validation logic here
+    // For example, check if all required fields are filled
+    const isAllFieldsValid = Array.from(formOfInps.current.elements).every(
+      (element) => {
+        return !element.required || element.value.trim() !== "";
+      }
+    );
+    setIsFormValid(isAllFieldsValid);
+
+    if (isAllFieldsValid) {
+      // Submit the form if all fields are valid
+      formOfInps.current.submit();
+    } else {
+      alert("Please fill out all required fields.");
+    }
+  };
+  const onSubmit = (e) =>{
+    e.preventDefault();
+  }
   return (
     <div className="IPN_container  main_content_wrapper">
       <div className="title">
@@ -19,7 +41,11 @@ const InputPlayerNames = () => {
       <div className="input_text">
         <h2>შეიყვანეთ მოთამაშეების სახელები</h2>
       </div>
-      <div className="inputs">{inputComponents}</div>
+      <div className="inputs">
+        <form ref={formOfInps} onSubmit={onSubmit} >
+          {inputComponents}
+        </form>
+      </div>
       {/* circles of steps */}
       <div className="prev_next_btn">
         <div className="prev_btn">
@@ -29,9 +55,15 @@ const InputPlayerNames = () => {
             </button>
           </Link>
         </div>
+        <button
+          className="submit_btn"
+          onClick={handleSubmit}
+        >
+          შენახვა
+        </button>
         <div className="next_btn">
           <Link to={"/input_player_roles"}>
-            <button>
+            <button> {/* RAGACEBS VCHALICHOB */}
               <p>შემდეგი</p>
             </button>
           </Link>
