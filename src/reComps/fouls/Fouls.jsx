ea55465a1aa378b610d3f217646d61d4deb07446
archiltavdgiridze <achitavdgiridze@gmail.com>
2023-09-26 22:37:57 +0400

    added logic to foul module that changes isAlive and isDeadForever states when user reaches maximum foul quantity and it can also be reverted. also in the proccess of adding expanded docsaves logic

--- src/reComps/fouls/Fouls.jsx
+++ src/reComps/fouls/Fouls.jsx
@@ -9,6 +9,42 @@ const Fouls = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const players = JSON.parse(sessionStorage.getItem("playerNames"));
+  const foulQuantity = JSON.parse(sessionStorage.getItem("foulQuantity"));
+  const foulCount = JSON.parse(sessionStorage.getItem("foulCounts"));
+  const gameData = JSON.parse(sessionStorage.getItem("gameData"));
+
+  // Initialize player statuses and store them in state
+  const initialPlayerStatus = gameData.map((data) => ({
+    isAlive: data.playerState.isAlive,
+    isDeadForever: data.playerState.isDeadForever,
+  }));
+
+  const [playerStatuses, setPlayerStatuses] = useState(initialPlayerStatus);
+
+  // Monitor foul counts and update player statuses
+  useEffect(() => {
+    for (let i = 0; i < foulCount.length; i++) {
+      if (foulCount[i] === foulQuantity) {
+        // Player reached foulQuantity, set status to dead forever
+        gameData[i].playerState.isAlive = false;
+        gameData[i].playerState.isDeadForever = true;
+      } else {
+        // Player's foul count is less than foulQuantity
+        // Check if they were dead forever and revert if necessary
+        if (playerStatuses[i].isDeadForever) {
+          gameData[i].playerState.isAlive = true;
+          gameData[i].playerState.isDeadForever = false;
+        }
+      }
+    }
+    sessionStorage.setItem("gameData", JSON.stringify(gameData));
+    setPlayerStatuses(
+      gameData.map((data) => ({
+        isAlive: data.playerState.isAlive,
+        isDeadForever: data.playerState.isDeadForever,
+      }))
+    );
+  }, [foulCount]);
 
   // Initialize foulCounts from session storage or with zeros if not available
   const initialFoulCounts =
@@ -25,7 +61,9 @@ const Fouls = () => {
 
   const handleFoulAdd = (index) => {
     const newFoulCounts = [...foulCounts];
-    newFoulCounts[index] += 1;
+    if (newFoulCounts[index] < foulQuantity) {
+      newFoulCounts[index] += 1;
+    }
     setFoulCounts(newFoulCounts);
   };
 
